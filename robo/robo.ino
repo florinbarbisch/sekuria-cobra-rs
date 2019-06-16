bcab67f24dc386801f3c3e7766f9e641179e85a0
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2019-06-16 15:55:03 +0200

    more sketches

--- robo/robo.ino
+++ robo/robo.ino
@@ -17,13 +17,6 @@ int d1 = 5;
 int d2 = 6;
 int d3 = 7;
 
-bool fullstep[4][4] = {
-  {1,1,0,0},
-  {1,0,0,1},
-  {0,0,1,1},
-  {0,1,1,0}
-};
-
 bool halfstep[8][4] = {
   {1,1,0,0},
   {1,1,0,1},
@@ -49,14 +42,44 @@ void setup() {
 }
 
 void loop() {
-  
+    
 }
 
-void step(bool forward, bool halfstep, int frequency) {
+// Moves the given motor one step. 
+// The direction, if it should be a halfstep and the frequency can be given.
+//
+// The motor is addressed as follows:
+// # | binary | axis
+// 0 | 0b000  | no motor moves, but all motors will receive current, which results in heating up the motors very quicly, NOT RECOMMENDED!
+// 1 | 0b001  | wrist right
+// 2 | 0b010  | upper arm
+// 3 | 0b011  | gripper
+// 4 | 0b100  | shoulder rotate
+// 5 | 0b101  | wrist left
+// 6 | 0b110  | under arm
+// 7 | 0b111  | no motor moves, an no motor will receive current, this is best for idle.
+// 
+// If forward is true, the motor will rotate forward.
+//
+// If halfstep is false, the motor tries to make a fullstep (equal to 2 halfsteps). 
+// But if the motor is still in a half step which does not match a fullstep, it will only make a halfstep, 
+// the position will then match a full step.
+//
+// The frequency determines how many steps the robot performs per minute.
+void step(int motor, bool forward, bool halfstep, int frequency) {
+  
   int delta = halfstep ? 1 : i % 2 == 1 ? 1 : 2;
-  if (!forward) delta *= -1;
+  if (!forward) { 
+    delta *= -1; 
+  }
   int i = (i + delta) % 8;
+  
   digitalWrite(transmissionDirection, 0);
+  
+  digitalWrite(m0, (n & ( 1 << 2 )) >> 2);
+  digitalWrite(m1, (n & ( 1 << 1 )) >> 1);
+  digitalWrite(m2, (n & ( 1 << 0 )) >> 0);
+  
   digitalWrite(d3, halfstep[i][0]);
   digitalWrite(d2, halfstep[i][1]);
   digitalWrite(d1, halfstep[i][2]);
